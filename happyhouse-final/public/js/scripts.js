84319e7b054cc849752080df754e4ee800f0b0c5
jang138 <david9696@naver.com>
2022-05-20 11:18:20 +0900

    navbar

--- happyhouse-final/public/js/scripts.js
+++ happyhouse-final/public/js/scripts.js
@@ -6,7 +6,7 @@
 //
 // Scripts
 //
-
+console.log("aaa");
 window.addEventListener("DOMContentLoaded", (event) => {
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector("#sidebarToggle");
--- happyhouse-final/src/components/NavBar.vue
+++ happyhouse-final/src/components/NavBar.vue
@@ -218,10 +218,7 @@
         </div>
     </div>
 </template>
-<script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    crossorigin="anonymous"
-></script>
+
 <script
     src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"
     crossorigin="anonymous"
@@ -238,6 +235,7 @@
     crossorigin="anonymous"
 ></script>
 <script src="../js/scripts.js"></script>
+
 <script>
 export default {};
 </script>
