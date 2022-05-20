275d9c840b67d8286094c760a32bd0874f1ff964
박기수 <rltn2121@naver.com>
2022-05-20 13:50:03 +0900

    test

--- happyhouse-final/src/App.vue
+++ happyhouse-final/src/App.vue
@@ -3,6 +3,7 @@
     <!-- <Login></Login>
     <Home></Home> -->
     <apt-detail-modal-test></apt-detail-modal-test>
+    <!-- <nav-bar></nav-bar> -->
   </div>
 </template>
 
@@ -11,7 +12,7 @@ import Login from "./components/Login.vue";
 import Home from "./components/Home.vue";
 import Market from "./components/apt/Market.vue";
 import AptDetailModalTest from "./components/test/AptDetailModalTest.vue";
-
+import NavBar from "./components/NavBar.vue";
 export default {
   name: "App",
   components: {
@@ -19,6 +20,7 @@ export default {
     Home,
     Market,
     AptDetailModalTest,
+    NavBar,
   },
 };
 </script>
